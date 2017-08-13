c2420ed5e99e85c6eb560e249e7f5d0eca491b7e
charleslipku <charleslipku@gmail.com>
2017-08-13 17:08:38 -0500

    update comments for simple mnist

--- simple_mnist.py
+++ simple_mnist.py
@@ -11,7 +11,7 @@ W = tf.Variable(tf.zeros([784,10]))
 b = tf.Variable(tf.zeros([10]))
 label = tf.placeholder(dtype=tf.float32,shape=[None,10])
 
-# define the model
+# define the model, loss function and optimization method
 y = tf.nn.softmax(tf.matmul(x,W)+b)
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(label*tf.log(y),reduction_indices=[1]))
 train_step = tf.train.GradientDescentOptimizer(learning_rate=0.02).minimize(cross_entropy)
@@ -25,6 +25,7 @@ for i in range(1000):
 	batch_xs, batch_ys = mnist.train.next_batch(100)
 	sess.run(train_step, feed_dict={x: batch_xs, label:batch_ys})
 
+# evaluate the model
 correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(label,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 print(sess.run(accuracy, feed_dict={x: mnist.test.images, label: mnist.test.labels}))
